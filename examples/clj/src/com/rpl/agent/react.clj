743a5b4a8c77fbc2e5b0dd1f3f4ae0d41804637b
Nathan Marz <nathan@redplanetlabs.com>
2025-08-29 11:40:12 -1000

    clean up ReAct agent

--- examples/clj/src/com/rpl/agent/react.clj
+++ examples/clj/src/com/rpl/agent/react.clj
@@ -29,23 +29,18 @@
       (.excludeDomains ["en.wikipedia.org"])
       .build))
 
-(defn- mk-tavily-search [{:keys [max-results] :or {max-results 3}}]
-  (fn tavily-search
-    [agent-node _ arguments]
-    (let [terms                         (get arguments "terms")
-          ^TavilyWebSearchEngine tavily (aor/get-agent-object
-                                         agent-node
-                                         "tavily")
-          search-results                (WebSearchRequest/from
-                                         terms
-                                         (int max-results))]
+(defn- mk-tavily-search
+  [{:keys [max-results] :or {max-results 3}}]
+  (fn [agent-node _ arguments]
+    (let [terms          (get arguments "terms")
+          tavily         (aor/get-agent-object agent-node "tavily")
+          search-results (WebSearchRequest/from terms (int max-results))]
       (str/join
        "\n---\n"
        (mapv
-        (fn [^Document doc]
-          (.text doc))
+        (fn [^Document doc] (.text doc))
         (.toDocuments
-         (.search tavily search-results)))))))
+         (.search ^TavilyWebSearchEngine tavily search-results)))))))
 
 (def ^:private TOOLS
   "Description of available tools"
@@ -66,74 +61,60 @@
    topology
    "openai-api-key"
    (System/getenv "OPENAI_API_KEY"))
-
   (aor/declare-agent-object
    topology
    "tavily-api-key"
    (System/getenv "TAVILY_API_KEY"))
-
   (aor/declare-agent-object-builder
    topology
    "openai"
-   (fn chat-model [setup]
-     ;; NOTE Using non-streaming model as 1.2 seems to have an issue wrapping
-     ;; ToolExecutionRequest for the return value from the chat.
+   (fn [setup]
      (-> (OpenAiChatModel/builder)
          (.apiKey (aor/get-agent-object setup "openai-api-key"))
          (.modelName "gpt-4o-mini")
          .build)))
-
   (aor/declare-agent-object-builder
    topology
    "tavily"
-   (fn tavily [setup]
+   (fn [setup]
      (tavily-web-search-engine (aor/get-agent-object setup "tavily-api-key"))))
-
+  (tools/new-tools-agent topology "tools" TOOLS)
   (->
-   topology
-   (aor/new-agent "ReActAgent")
-
-   (aor/node
-    "chat"
-    "chat"
-    (fn chat-fn [agent-node messages]
-      (let [openai     (aor/get-agent-object agent-node "openai")
-            tools      (aor/agent-client agent-node "tools")
-            response   (lc4j/chat
-                        openai
-                        (lc4j/chat-request messages {:tools TOOLS}))
-            ai-message (.aiMessage response)
-            tool-calls (not-empty (vec (.toolExecutionRequests ai-message)))]
-        (if tool-calls
-          (let [tool-results  (aor/agent-invoke tools tool-calls)
-                next-messages (into (conj messages ai-message) tool-results)]
-            (aor/emit! agent-node "chat" next-messages))
-          (aor/result! agent-node (.text ai-message)))))))
-
-  (tools/new-tools-agent topology "tools" TOOLS))
-
-;;; Example Invocation
-
-(def ^:private SYSTEM-PROMPT "You are a helpful AI assistant.
-
-System time: %s")
+    topology
+    (aor/new-agent "ReActAgent")
+    (aor/node
+     "chat"
+     "chat"
+     (fn [agent-node messages]
+       (let [openai     (aor/get-agent-object agent-node "openai")
+             tools      (aor/agent-client agent-node "tools")
+             response   (lc4j/chat
+                         openai
+                         (lc4j/chat-request messages {:tools TOOLS}))
+             ai-message (.aiMessage response)
+             tool-calls (vec (.toolExecutionRequests ai-message))]
+         (if (not-empty tool-calls)
+           (let [tool-results  (aor/agent-invoke tools tool-calls)
+                 next-messages (into (conj messages ai-message) tool-results)]
+             (aor/emit! agent-node "chat" next-messages))
+           (aor/result! agent-node (.text ai-message))))))))
 
 (defn run-agent
   []
   (with-open [ipc (rtest/create-ipc)
-              _   (aor/start-ui ipc)]
+              _ (aor/start-ui ipc)]
     (rtest/launch-module! ipc ReActModule {:tasks 4 :threads 2})
     (let [module-name   (rama/get-module-name ReActModule)
           agent-manager (aor/agent-manager ipc module-name)
           agent         (aor/agent-client agent-manager "ReActAgent")
-          _             (print "Ask your question (agent has web search access): ")
-          _             (flush)
-          user-input    (read-line)
+          _ (print "Ask your question (agent has web search access): ")
+          _ (flush)
+          ^String user-input (read-line)
           result        (aor/agent-invoke
                          agent
                          [(SystemMessage/from
                            (format
-                            SYSTEM-PROMPT
+                            "You are a helpful AI assistant. System time: %s"
                             (.toString (java.time.Instant/now))))
                           (UserMessage. user-input)])]
       (println result))))
